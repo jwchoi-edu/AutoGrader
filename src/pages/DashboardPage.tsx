df7ae24296696538c49ec1f32458dbacef65296c
Jongwon Choi <h2511129@joongdong.hs.kr>
2026-05-31 19:48:36 +0900

    feat: add logout icon

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -109,6 +109,10 @@ export function DashboardPage({
           void onSignOut()
         }}
       >
+        <svg className="inline-block mr-2 -ml-1 h-4 w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden>
+          <path d="M16 17l5-5-5-5M21 12H9" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M13 19H7a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h6" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
         로그아웃
       </button>
     </div>
